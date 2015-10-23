5c72b27f89748fccdd9f85f030e22e182fd1eb70
kymbrik <kymbrik3@rambler.ru>
2015-10-24 00:03:21 +0300

    5 commit

--- DB/firstwebpagedb.sql
+++ DB/firstwebpagedb.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Хост: 127.0.0.1
--- Время создания: Окт 24 2015 г., 00:51
+-- Время создания: Окт 24 2015 г., 01:01
 -- Версия сервера: 5.1.51-community
 -- Версия PHP: 5.3.13
 
@@ -55,6 +55,19 @@ INSERT INTO `texts` (`id_text`, `title`, `content`) VALUES
 (2, 'Обо мне', 'Содержимое страницы Обо мне'),
 (3, 'Фотогалерея', 'Содержимое страницы Фотогалерея'),
 (4, 'Контакты', '+ 79254466612');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `id_user` int(10) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  PRIMARY KEY (`id_user`)
+) ENGINE=InnoDB DEFAULT CHARSET=cp1251 AUTO_INCREMENT=1 ;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
